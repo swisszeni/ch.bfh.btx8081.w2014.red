3066d04669f2b0e698f045201b38ab59de635b37
zauga5 <zauga5@bfh.ch>
2014-11-27 16:09:49 +0100

    create Subclass Doctor

---
+++ src/main/java/models/Doctor.java
@@ -0,0 +1,53 @@
+package models;
+
+import java.util.GregorianCalendar;
+
+public class Doctor extends Person {
+	
+	private String zsrNr;
+	private String specialication;
+	private Address workingAddress;
+
+	public Doctor(String lastName, String firstName, Address address,
+			GregorianCalendar birthday, String email, String businessNr,
+			String privateNr, String mobileNr, String pictureUri, String zsrNr, String specialication, 
+			Address workingAddress) 
+	{
+		super(lastName, firstName, address, birthday, email, businessNr, privateNr,
+				mobileNr, pictureUri);
+		// TODO Auto-generated constructor stub
+		
+		this.zsrNr = zsrNr;
+		this.specialication = specialication;
+		this.workingAddress = workingAddress;
+		
+		
+		
+	}
+
+	public String getZsrNr() {
+		return zsrNr;
+	}
+
+	public void setZsrNr(String zsrNr) {
+		this.zsrNr = zsrNr;
+	}
+
+	public String getSpecialication() {
+		return specialication;
+	}
+
+	public void setSpecialication(String specialication) {
+		this.specialication = specialication;
+	}
+
+	public Address getWorkingAddress() {
+		return workingAddress;
+	}
+
+	public void setWorkingAddress(Address workingAddress) {
+		this.workingAddress = workingAddress;
+	}
+
+	
+}
