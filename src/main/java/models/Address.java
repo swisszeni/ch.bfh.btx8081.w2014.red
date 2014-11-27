f6e4635473e09764c45bba3b3020336765dfdd0d
zauga5 <zauga5@bfh.ch>
2014-11-27 15:20:57 +0100

    class Address

---
+++ src/main/java/models/Address.java
@@ -0,0 +1,26 @@
+package models;
+
+/* Defines street, zipcode, city, country of a specified Address */
+
+public class Address {
+	
+	private String street;
+	private String zip;
+	private String city;
+	private String country;
+	
+	/* creates an object of address class */
+	public Address ( String street, String zip, String city, String country )
+	
+	{
+		this.street = street;
+		this.zip = zip;
+		this.city =  city;
+		this.country = country;
+		
+	}
+	
+	
+	
+
+}
