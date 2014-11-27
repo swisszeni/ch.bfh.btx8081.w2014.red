c498dce6f7804a0a7a40c73474dc0642651e45f3
zauga5 <zauga5@bfh.ch>
2014-11-27 15:25:36 +0100

    class Address

--- src/main/java/models/Address.java
+++ src/main/java/models/Address.java
@@ -19,6 +19,38 @@ public class Address {
 		this.country = country;
 		
 	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
 	
 	
 	
