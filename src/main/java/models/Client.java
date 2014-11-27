087fc838cda0d724c12baaa02ae67c3dd7a6d2e4
zauga5 <zauga5@bfh.ch>
2014-11-27 16:03:43 +0100

    create Subclass Client

---
+++ src/main/java/models/Client.java
@@ -0,0 +1,51 @@
+package models;
+
+import java.util.GregorianCalendar;
+
+
+public class Client extends Person {
+	
+	private boolean harmless;
+	private Person emergencyContact;
+	private Doctor doc;
+
+	public Client(String lastName, String firstName, Address address,
+			GregorianCalendar birthday, String email, String businessNr,
+			String privateNr, String mobileNr, String pictureUri, boolean harmless, Person emergencyContact, Doctor doc) {
+		super(lastName, firstName, address, birthday, email, businessNr, privateNr,
+				mobileNr, pictureUri);
+		// TODO Auto-generated constructor stub
+	
+		this.harmless = harmless;
+		this.emergencyContact = emergencyContact;
+		this.doc = doc;
+		
+	}
+
+	public boolean isHarmless() {
+		return harmless;
+	}
+
+	public void setHarmless(boolean harmless) {
+		this.harmless = harmless;
+	}
+
+	public Person getEmergencyContact() {
+		return emergencyContact;
+	}
+
+	public void setEmergencyContact(Person emergencyContact) {
+		this.emergencyContact = emergencyContact;
+	}
+
+	public Doctor getDoc() {
+		return doc;
+	}
+
+	public void setDoc(Doctor doc) {
+		this.doc = doc;
+	}
+	
+	
+
+}
