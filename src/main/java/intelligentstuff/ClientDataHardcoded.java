36e951b3a0ca1f1d7a4977e4d374d8ea2b1843f1
Raphael <raphael@zenhausern.net>
2014-12-04 07:56:18 +0100

    Implemented some of the stub methods

--- src/main/java/intelligentstuff/ClientDataHardcoded.java
+++ src/main/java/intelligentstuff/ClientDataHardcoded.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.GregorianCalendar;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Predicate;
-
 import models.Address;
 import models.Client;
 import models.Person;
@@ -24,7 +21,7 @@ public class ClientDataHardcoded implements ClientDataSource {
 				null, null, null,
 				true, null, null));
 		
-		clients.add(new Client(13455, "Mosimann", "Sophie", new Address("Quellgasse", "2500", "Biel", "Schweiz"), new GregorianCalendar(1900, 01, 01), "so@gmx.ch", "002493938", "8437894839", "079585858", "www.bild.ch", false, null, null));
+		clients.add(new Client(13455, "Heinzmann", "Steffie", new Address("Penisstrasse", "9001", "Bern", "Schweiz"), new GregorianCalendar(1900, 01, 01), "sokm@gmx.ch", "002493938", "8437894839", "079585858", "www.bild.ch", false, null, null));
 		clients.add(new Client(888, "Mosimann", "Sophie", new Address("Quellgasse", "2500", "Biel", "Schweiz"), new GregorianCalendar(1990, 01, 01), "so@gmx.ch", "002493938", "8437894839", "079585858", "www.bild.ch",true, new Person(889, "Notfall", "Kontakt", new Address("Strasse", "Z1P", "Staaadt", "Laaaand"), null, null, null, null, null, null), null));
 	}
 	
@@ -56,8 +53,8 @@ public class ClientDataHardcoded implements ClientDataSource {
 
 	@Override
 	public List<Client> getClients(int offset, int limit) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Client> cs = clients.subList(offset, limit);
+		return cs;
 	}
 
 	@Override
@@ -68,8 +65,14 @@ public class ClientDataHardcoded implements ClientDataSource {
 
 	@Override
 	public List<Client> searchClientsByLastName(String lastName) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Client> cs = new ArrayList<Client>();
+		for(Client c : clients) {
+			if(c.getLastName().equals(lastName)) {
+				cs.add(c);
+			}
+		}
+		
+		return cs;
 	}
 
 }
