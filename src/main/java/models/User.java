7e80104083893471a8629b3640fe342240a525c5
Flo <schnf8@bfh.ch>
2014-12-17 13:54:42 +0100

    Extended the user class with the person class

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,24 +1,35 @@
 package models;
 
-public class User {
-	
+import java.util.Date;
+
+public class User extends Person {
+
 	private String username;
 	private String password;
-	
-	public User(String username, String password){
+
+	public User(int personId, String lastName, String firstName,
+			Address address, Date birthday, String email, String businessNr,
+			String privateNr, String mobileNr, String pictureUri,
+			String username, String password) {
+		super(personId, lastName, firstName, address, birthday, email,
+				businessNr, privateNr, mobileNr, pictureUri);
+
 		this.username = username;
 		this.password = password;
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
