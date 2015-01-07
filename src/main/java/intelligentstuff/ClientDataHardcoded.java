26915eb381110890c52fdb3e189eabf12afd805b
Flo <schnf8@bfh.ch>
2015-01-07 17:12:14 +0100

    Changed a not allowed street name for an hard coded user

--- src/main/java/intelligentstuff/ClientDataHardcoded.java
+++ src/main/java/intelligentstuff/ClientDataHardcoded.java
@@ -30,7 +30,7 @@ public class ClientDataHardcoded implements ClientDataSource {
 				null, "muster@mail.ch", null,
 				null, null, null,
 				true, fry, zoid));
-		clients.add(new Client(13455, "Heinzmann", "Steffie", new Address("Penisstrasse", "9001", "Bern", "Schweiz"), new Date(1900, 01, 01), "sokm@gmx.ch", "002493938", "8437894839", "079585858", "www.bild.ch", false, emer2, zoid));
+		clients.add(new Client(13455, "Heinzmann", "Steffie", new Address("Münstergasse 34", "9001", "Bern", "Schweiz"), new Date(1900, 01, 01), "sokm@gmx.ch", "002493938", "8437894839", "079585858", "www.bild.ch", false, emer2, zoid));
 		clients.add(new Client(888, "Mosimann", "Sophie", new Address("Quellgasse", "2500", "Biel", "Schweiz"), new Date(1990, 01, 01), "so@gmx.ch", "002493938", "8437894839", "079585858", "www.bild.ch",true, emer1, fry));
 	}
 	
