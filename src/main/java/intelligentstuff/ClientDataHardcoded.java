18f3d92713b3ca742267d79255a6d50b197e745f
Flo <schnf8@bfh.ch>
2015-01-10 14:18:43 +0100

    Exchanged the picutre URI of each client with a real URI to a default picutre on Github.

--- src/main/java/intelligentstuff/ClientDataHardcoded.java
+++ src/main/java/intelligentstuff/ClientDataHardcoded.java
@@ -26,13 +26,10 @@ public class ClientDataHardcoded implements ClientDataSource {
 		Person emer1 = new Person(889, "Notfall", "Kontakt", new Address("Strasse", "Z1P", "Staaadt", "Laaaand"), null, null, null, null, null, null);
 		Person emer2 = new Person(756769, "Leela", "Turanga", null, null, "leela@deliveryexpress.com", null, null, null, null);
 		
-		clients.add(new Client(1, "Muster", "Hans", null,
-				null, "muster@mail.ch", null,
-				null, null, null,
-				true, fry, zoid));
-		clients.add(new Client(13455, "Heinzmann", "Steffie", new Address("Münstergasse 34", "9001", "Bern", "Schweiz"), new Date(1900, 01, 01), "sokm@gmx.ch", "002493938", "8437894839", "079585858", "www.bild.ch", false, emer2, zoid));
-		clients.add(new Client(888, "Mosimann", "Sophie", new Address("Quellgasse", "2500", "Biel", "Schweiz"), new Date(1990, 01, 01), "so@gmx.ch", "002493938", "8437894839", "079585858", "www.bild.ch",true, emer1, fry));
-		clients.add(new Client(6, "Sasha", "Grey", new Address("Tubestreet", "6666", "Interwebs", "United States"), new Date(1988, 01, 01), "sg@gmx.ch", "002493938", "8437894839", "079585858", "www.bild.ch",true, emer1, zoid));
+		clients.add(new Client(1, "Muster", "Hans", null,null, "muster@mail.ch", null, null, null, "https:/raw.githubusercontent.com/swisszeni/ch.bfh.btx8081.w2014.red/master/doc/task11/default.jpg", true, fry, zoid));
+		clients.add(new Client(13455, "Heinzmann", "Steffie", new Address("Münstergasse 34", "9001", "Bern", "Schweiz"), new Date(1900, 01, 01), "sokm@gmx.ch", "002493938", "8437894839", "079585858", "https:/raw.githubusercontent.com/swisszeni/ch.bfh.btx8081.w2014.red/master/doc/task11/default.jpg", false, emer2, zoid));
+		clients.add(new Client(888, "Mosimann", "Sophie", new Address("Quellgasse", "2500", "Biel", "Schweiz"), new Date(1990, 01, 01), "so@gmx.ch", "002493938", "8437894839", "079585858", "https:/raw.githubusercontent.com/swisszeni/ch.bfh.btx8081.w2014.red/master/doc/task11/default.jpg",true, emer1, fry));
+		clients.add(new Client(6, "Sasha", "Grey", new Address("Tubestreet", "6666", "Interwebs", "United States"), new Date(1988, 01, 01), "sg@gmx.ch", "002493938", "8437894839", "079585858", "https:/raw.githubusercontent.com/swisszeni/ch.bfh.btx8081.w2014.red/master/doc/task11/default.jpg",true, emer1, zoid));
 	}
 	
 	public static ClientDataHardcoded getInstance () {
